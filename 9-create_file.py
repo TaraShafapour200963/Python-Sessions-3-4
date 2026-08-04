0de5af69dc3671c235b90b1168edcc955e28937a
TaraShafapour <titi200963@gmail.com>
2026-08-04 23:04:10 +0330

    Delete 9-create_file.py

--- 9-create_file.py
+++
@@ -1,7 +0,0 @@
-with open("students.txt","w",encoding="UTF-8") as file:
-    file.write("tara\n")
-    file.write("mobina\n")
-    file.write("sara\n")
-with open("students.txt","r",encoding="UTF-8") as file:
-    data=file.read()
-    print(data)
